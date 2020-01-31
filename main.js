362b58458cde534327890ea7dcd773324d0e1c9d
Asif <rmasif@gmail.com>
2020-01-31 12:28:37 +0500

    updated

--- main.js
+++ main.js
@@ -8,7 +8,25 @@
         project : "socks",
         description : "product description",
         image: "assets/socks.png",
-        image1: "assets/blue_socks.png"
+        image1: "assets/blue_socks.png",
+        inStock: true,
+        numval : 5,
+        details : ["80% Cottons","20 Polyester","Gender Neutral"],
+        variants :[{id:01,color:"red"},{id:02,color:"blue"},{id:03,color:"green"},{id:04,color:"yellow"}]
+
     }
     
 });
+
+
+
+/*
+
+conditions
+---------------
+v-if
+v-else-if
+v-else
+v-show
+
+*/
